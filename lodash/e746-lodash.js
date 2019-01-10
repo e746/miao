132c33711c7f88c3c6d6a70ef0e3c68ea7a04dfe
unknown <373492229@qq.com>
2019-01-10 12:11:39 +0800

    2

--- lodash/e746-lodash.js
+++ lodash/e746-lodash.js
@@ -1,14 +1,29 @@
-
-var e746 =function(){
- function join(arr,sep = ","){
+var e746 = function(){
+  function join(arr,sep = ","){
     var str=""
-    for(i=0;i<arr.length){
+    for(let i=0;i<arr.length;i++){
       str+=arr[i]
       if(i!=arr.length-1) str+=sep
     }
-  return str
- }
-  return {
-     join:join,
+    return str
   }
+
+  return {
+     join: join,
+  } 
 }()
+
+var chenximan = function(){
+
+    function chunk(ary, size = 1) {
+  
+    }
+    function compact(ary) {
+  
+    }
+  
+    return {
+      chunk: chunk,
+      compact: compact,
+    }
+  }()
